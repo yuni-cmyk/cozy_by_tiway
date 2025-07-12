c871b4d212d84f40ff017168e50ae4ccda66485e
melanieaulia <melanieaulia@example.com>
2025-07-12 21:02:13 +0700

    Deploy: Add Dockerfile, nginx, supervisor for Railway production

--- app/Models/produk.php
+++ app/Models/produk.php
@@ -1,17 +1,1 @@
-<?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class Produk extends Model
-{
-    use HasFactory;
-
-    protected $fillable = [
-        'nama_baju',
-        'harga',
-        'gambar'
-    ];
-}
+ 
